c48059a668c949580734c9ce0075feee14c7ea04
Dheeraj Arora <dheeraj.arora1997@yahoo.com>
2022-08-05 23:18:56 +0530

    base setup

---
+++ src/Components/SearchBox.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default function SearchBox() {
+ return (
+  <>
+   <h1 className="text-primary">
+    Hello world!
+   </h1>
+  </>
+ )
+}
---
+++ src/Landing.jsx
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import SearchBox from './Components/SearchBox';
+export default function Landing() {
+ return (<SearchBox />)
+}
